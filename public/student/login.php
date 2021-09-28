d9265a3fd0bed69abb71365012fc8603f3858bad
chukwuemekasantos <chukwuanthony544@gmail.com>
2021-09-28 15:56:02 +0100

    login and signup task

--- public/student/login.php
+++ public/student/login.php
@@ -38,10 +38,10 @@ if (!checkStudentLogin()) {
             <div class="container-fluid h-custom">
                 <div class="row d-flex justify-content-center align-items-center h-100">
                     <div class="col-md-9 col-lg-6 col-xl-5">
-                        <img src="./images/auth.png" class="img-fluid" alt="Sample image">
+                        <img src="../clearance-app/public/images/auth.png" class="img-fluid" alt="Sample image">
                     </div>
                     <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                        <form method="POST" action="/student/login.php">
+                        <form method="POST" action="./login.php">
                             <div class="flex-row d-flex align-items-center justify-content-center justify-content-lg-start">
                                 <h2 class="mb-4 me-3">Login</h2>
                             </div>
